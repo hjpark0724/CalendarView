8e6c9bd4c05d27f8b6cabfe4bef233b76e304e4c
basker <social724@gmail.com>
2023-05-17 09:58:15 +0900

    Initial Commit

---
+++ CalendarView/CalendarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewApp.swift
+//  CalendarView
+//
+//  Created by HYEONJUN PARK on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct CalendarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
